4ea17de7b5d6ee90a790d7d0ed9893dd32a8e265
user <user@TapasRout>
2023-06-06 12:39:25 +0530

    Update

--- MiniProject-03-SSN_Social-Security-Number_RestAPI/src/test/java/fis/pss/MiniProject03SsnSocialSecurityNumberRestApiApplicationTests.java
+++ MiniProject-03-SSN_Social-Security-Number_RestAPI/src/test/java/fis/pss/MiniProject03SsnSocialSecurityNumberRestApiApplicationTests.java
@@ -1,7 +1,15 @@
 package fis.pss;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 class MiniProject03SsnSocialSecurityNumberRestApiApplicationTests {
@@ -10,4 +18,16 @@ class MiniProject03SsnSocialSecurityNumberRestApiApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private MockMvc mockMvc;
+	
+	
+	@Test
+	 void customerRegistrationTest() throws Exception {
+		MockHttpServletRequestBuilder  request = MockMvcRequestBuilders.get("/getUser/10005");
+		MvcResult result = mockMvc.perform(request).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		int status = response.getStatus();
+		assertEquals(200, status);
+	}
 }

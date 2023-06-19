5315b82607acf54e66ddc077bb8da53d34a548fe
user <user@TapasRout>
2023-06-19 20:57:47 +0530

    update

--- MainProject-SpringBoot-HIS_Health_Insurance_System/src/main/java/fis/his/co/batach/controller/BatchProcessingController.java
+++ MainProject-SpringBoot-HIS_Health_Insurance_System/src/main/java/fis/his/co/batach/controller/BatchProcessingController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fis.his.admin.case_workers_management.constant.LogConstant;
 import fis.his.co.batach.process.BatchProcessing;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/batch")
+@Slf4j
 public class BatchProcessingController {
 
 	@Autowired
@@ -16,10 +19,15 @@ public class BatchProcessingController {
 	
 	@GetMapping("/pdf")
 	public void getPdf(){
+		log.info(LogConstant.METHOD_EXECUTION_STARTED+"-Batch pdf gen");
 		try {
+			log.debug("Debug started in pdf genration");
 			batch.test();
+			log.debug("pdf generation ended");
 		} catch (NumberFormatException | ClassNotFoundException e) {
+			log.error("Problem in pdf generation"+e.getMessage());
 			e.printStackTrace();
 		}
+		log.info(LogConstant.METHOD_EXECUTION_ENDED);
 	}
 }
--- MainProject-SpringBoot-HIS_Health_Insurance_System/src/main/java/fis/his/dc/controller/plancontroller/PlanController.java
+++ MainProject-SpringBoot-HIS_Health_Insurance_System/src/main/java/fis/his/dc/controller/plancontroller/PlanController.java
@@ -13,14 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import static fis.his.admin.case_workers_management.constant.LogConstant.*;
 import fis.his.admin.planmngmt.service.PlanServiceInterface;
 import fis.his.application_registration.model.ARModel;
 import fis.his.application_registration.service.ARServiceInterface;
 import fis.his.dc.model.DcPlanModel;
 import fis.his.dc.service.dcplanservice.DcplanServiceInterface;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/Dcplan")
+@Slf4j
 public class PlanController {
 
 	@Autowired
@@ -31,10 +34,19 @@ public class PlanController {
 	@Autowired
 	private PlanServiceInterface planservice;
 	
+	
+	/**
+	 * {@summary : Method is used for Show plan creation form}
+	 * @param model : DcPlanModel
+	 * @param id :Integer
+	 * @param map : ModelAndView
+	 * @return : String LVN
+	 */
 	@GetMapping("/dcplanhome")
 	public String showPlanCreateForm(@ModelAttribute("model") DcPlanModel model,
 			                         @RequestParam("id") Integer id,
 			                         Map<String, Object> map) {
+		log.info(METHOD_EXECUTION_STARTED+"-showPlanCreateForm");
 		ARModel armodel = arservice.fetchApplication(id);
 		List<String> list = planservice.getAllList()
 		           .stream()
@@ -42,16 +54,31 @@ public class PlanController {
 		           .toList();
 		BeanUtils.copyProperties(armodel, model);
 		map.put("rolelist", list);
+		log.info(METHOD_EXECUTION_ENDED);
 	 return "DCModule/Dcplan/chooseplan";	
 	}
 	
+	
+	/**
+	 * {@summary : Method is used for after post plan form}
+	 * @param model :DcPlanModel 
+	 * @param redirect :/Dcplan/childdcform
+	 * @return : String LVN
+	 */
 	@PostMapping("/postdcplanhome")
 	public String createPlan(@ModelAttribute("model") DcPlanModel model,
 			                 RedirectAttributes redirect) {
+		log.info(METHOD_EXECUTION_STARTED+"-createPlan");
 		String result = service.createPlan(model);
 		redirect.addFlashAttribute("result",result);
+		log.info(METHOD_EXECUTION_ENDED);
 		return "redirect:childdcform";
 	}
+	
+	/**
+	 * {@summary : Method is used for show child Data collection form}
+	 * @return : String logical view name
+	 */
 	@GetMapping("/childdcform")
 	public String showChildDcForm() {
 		return "DCModule/childdc/childdc";

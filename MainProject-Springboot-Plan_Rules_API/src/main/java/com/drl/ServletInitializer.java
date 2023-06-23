fe1abebc35fcc6aeb41567560bb62810b38aaf43
user <user@TapasRout>
2023-06-23 17:48:25 +0530

    update

--- MainProject-Springboot-Plan_Rules_API/src/main/java/com/drl/ServletInitializer.java
+++ MainProject-Springboot-Plan_Rules_API/src/main/java/com/drl/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.drools;
+package com.drl;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;

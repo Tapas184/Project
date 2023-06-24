7c77ffa8728cc4381643401d60c77a9331e159da
user <user@TapasRout>
2023-06-24 15:23:09 +0530

    u

---
+++ MainProject-Springboot-HIS-Plan_Rules-RestAPI/src/main/java/com/drools/constants/LogMsg.java
@@ -0,0 +1,17 @@
+package com.drools.constants;
+
+
+public class LogMsg {
+
+	private LogMsg() {
+	}
+public static final String METHOD_EXECUTION_STARTED="<< Method execution started>>";
+public static final String METHOD_EXECUTION_ENDED="<< Method execution ended>>";
+/*
+public static final String METHOD_EXECUTION_STARTED="<< Method execution started>>";
+public static final String METHOD_EXECUTION_STARTED="<< Method execution started>>";
+public static final String METHOD_EXECUTION_STARTED="<< Method execution started>>";
+public static final String METHOD_EXECUTION_STARTED="<< Method execution started>>";
+public static final String METHOD_EXECUTION_STARTED="<< Method execution started>>";
+*/	
+}
